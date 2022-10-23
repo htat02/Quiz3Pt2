ff921a0f20b25579ffda8d598dd7dcd88bef04ab
henry <htat@cpp.edu>
2022-10-23 12:40:40 -0700

    Initial outline

---
+++ main.c
@@ -0,0 +1,9 @@
+//use argc and argv to pass values throughout main function
+
+
+//create values of both tax and tip which can be entered through command line//
+
+//create constant meal cost of the 4 different foods
+
+//calculate from both tax and tip of the meal for total bill
+
