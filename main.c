840fb0fc2915f81df9ed0a7f7704f5c48870b385
henry <htat@cpp.edu>
2022-10-23 13:23:15 -0700

    Made random meal costs

--- main.c
+++ main.c
@@ -1,4 +1,28 @@
 //use argc and argv to pass values throughout main function
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(int argc, int *argv){
+
+float meal;
+
+srand(time(NULL));
+int r = rand()%4;
+if(r == 0){
+    meal = 9.95;
+}
+else if(r == 1){
+    meal = 4.55;
+}
+else if(r == 2){
+    meal = 13.25;
+}
+else if(r == 3){
+    meal = 22.35;
+}
+printf("The meal cost is %.2f\n", meal);
+
 
 
 //create values of both tax and tip which can be entered through command line//
